6f15278072a90de9fb26495bb20dc7a27b3c9483
raj <engineeringbuddy999@gmail.com>
2023-02-25 20:50:53 +0530

    added final components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import './footer.css'
+export default Footer
---
+++ src/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './portfolio.css'
+const Portfolio = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import './portfolio.css'
+export default Portfolio
