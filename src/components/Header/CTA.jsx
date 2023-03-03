e7369a052cd15d106fbda4fad79f7338ea5814e2
raj <engineeringbuddy999@gmail.com>
2023-03-03 21:45:01 +0530

    Lil changes

---
+++ src/components/Header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='CTA'>
+        <a href="">Download CV</a>
+        <a href="#contact">Let's Talk!</a>
+      
+    </div>
+  )
+}
+
+export default CTA
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div className="container header__container"></div>
+      <h5>Hello I'm</h5>
+      <h1>Raj Singh</h1>
+      <h5 className="text-light">FullStack Developer</h5>
+    </header>
   )
 }
 export default Header
