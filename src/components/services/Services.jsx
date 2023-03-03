7620a2e39308e46344fdfe9d878e6621e02b332c
raj <engineeringbuddy999@gmail.com>
2023-03-04 02:05:08 +0530

    nav component completed

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './services.css'
+
 const Services = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='services'>
+      Services
+    </section>
   )
 }
 
